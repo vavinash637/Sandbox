21138d25b827e1bb7c7da5c4649457047d10f71a
Avinash B V <vavinash@zeomega.com>
2016-02-03 13:30:29 +0530

    pushed init changes

--- client_jiva/Products/__init__.py
+++ client_jiva/Products/__init__.py
@@ -1,1 +1,33 @@
+#########################################################################
+#
+# ZeOmega Inc license
+# Copyright (c) 2003-2016 ZeOmega Inc
+# http://www.zeomega.com
+# All rights reserved.
+#
+# ZeOmega software [both binary and source (if released)] (hereafter,
+# Software) is intellectual property owned by ZeOmega Inc is copyright
+# of ZeOmega Inc in all countries in the world, and ownership remains
+# with ZeOmega Inc The Software is protected by the copyright laws of
+# the United States and international copyright treaties.  Licensee is
+# not allowed to distribute the binary and source code (if released) to
+# third parties.  Licensee is not allowed to reverse engineer,
+# disassemble or decompile code, or make any modifications of the binary
+# or source code, remove or alter any trademark, logo, copyright or
+# other proprietary notices, legends, symbols, or labels in the
+# Software.  Licensee is not allowed to sub-license the Software or any
+# derivative work based on or derived from the Software.  Neither the
+# names of ZeOmega Inc , nor the names of its contributors may be used
+# to endorse or promote products derived from this Software without
+# specific prior written permission.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+#
+#########################################################################
 __import__('pkg_resources').declare_namespace(__name__)
